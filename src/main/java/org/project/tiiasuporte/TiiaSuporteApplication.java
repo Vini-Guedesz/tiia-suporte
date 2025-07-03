77f3013d42245691f128d4257daa1461973309f0
Vinicius <marcosviniciosguedesdossantos@gmail.com>
2025-07-03 15:20:50 -0300

    Commit Inicial

---
+++ src/main/java/org/project/tiiasuporte/TiiaSuporteApplication.java
@@ -0,0 +1,22 @@
+package org.project.tiiasuporte;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title = "TIIA Suporte API",
+        version = "1.0",
+        description = "API de Ferramentas de Suporte Técnico de Rede"
+    )
+)
+public class TiiaSuporteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiiaSuporteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/tiiasuporte/TiiaSuporteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.tiiasuporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiiaSuporteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
